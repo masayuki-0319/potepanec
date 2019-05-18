4c119a0ffbc34dd57fe8da9c7c73dd5e8fba55d1
将之大西 <omccello@gmail.com>
2019-05-19 08:04:40 +0900

    RSpec：ProductsコントローラSpecを場所移動

---
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Potepan::ProductsController, type: :controller do
+  describe 'Product Controller' do
+    it "responds successfully" do
+      expect(response).to be_success
+    end
+
+    it "responds 'show' template " do
+      expect(response).to render_template :show
+    end
+  end
+end
