962feaa0bf5ce2c6467a346f12df72ee9ab3aeaf
将之大西 <omccello@gmail.com>
2019-05-19 21:09:20 +0900

    RSpec修正：require位置変更，テスト追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
-  describe 'Product Controller' do
-    it "responds successfully" do
-      expect(response).to be_success
+  describe '#show' do
+    subject { get :show, params: { id: product.id } }
+
+    let(:product) { FactoryBot.create(:product) }
+
+    it "レスポンスの確認" do
+      subject
+      expect(response).to be_successful
     end
 
-    # it "responds 'show' template " do
-    #   expect(response).to render_template :show
-    # end
+    it "View表示の確認" do
+      subject
+      expect(response).to render_template :show
+    end
+
+    it "インスタンスの確認" do
+      subject
+      expect(assigns(:product)).to eq(product)
+    end
   end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  # let(:product) { create(:product) }
-
+  before { FactoryBot.create(:product, name: "Test", price: 189, description: "description") }
   scenario "ユーザーがProduct詳細ページまでアクセスする。" do
     visit potepan_root_path
     expect(page).to have_title "BIGBAG Store"
     click_link "View Shirts"
     expect(page).to have_content "Product Grid Sidebar Left"
-    # click_link product.name
-    # expect(page).to have_content product.name
+    click_link "Test"
+    expect(page).to have_content "Test"
+    expect(page).to have_content 189
+    expect(page).to have_content "description"
   end
 end
