c126c537b1613a8bed568d564cf3642a526aa528
将之大西 <omccello@gmail.com>
2019-05-24 22:52:31 +0900

    最低限の商品分類表示を実装（要抽象化）

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,2 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
+  def show
+    @taxon = Spree::Taxon.find_by(id: params[:taxon_id])
+    @products = @taxon.products
+    render template: "potepan/sample/product_grid_left_sidebar"
+  end
 end
