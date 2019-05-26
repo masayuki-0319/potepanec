82a63edfdec02ff70be36dc5ba41c529b5d105dd
将之大西 <omccello@gmail.com>
2019-05-26 20:28:24 +0900

    テスト修正：カテゴリ用のfeatureスペックを作成

---
+++ spec/features/categories_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Categories", type: :feature do
+  subject do
+    product.taxons << taxon
+    taxonomy.root.children << taxon
+    other_product.taxons << other_taxon
+    other_taxonomy.root.children << other_taxon
+  end
+
+  let(:product) { create(:product) }
+  let(:taxon) { create(:taxon) }
+  let(:taxonomy) { create(:taxonomy) }
+  let(:other_product) { create(:product) }
+  let(:other_taxon) { create(:taxon) }
+  let(:other_taxonomy) { create(:taxonomy) }
+
+  scenario "商品カテゴリーの表示を確認する。" do
+    subject
+    visit potepan_category_path(taxon_id: taxon.id, view_type: 'Grid')
+    expect(page).to have_title taxon.name
+    expect(page).to have_content product.name
+    within('.side-nav') do
+      expect(page).to have_content taxonomy.name
+      expect(page).to have_content taxon.name
+      expect(page).to have_content taxon.product_ids.count
+      expect(page).to have_content other_taxonomy.name
+      expect(page).to have_content other_taxon.name
+      expect(page).to have_content other_taxon.product_ids.count
+    end
+  end
+end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,37 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  subject do
-    product.taxons << taxon
-    taxonomy.root.children << taxon
-  end
-
   let(:product) { create(:product) }
-  let(:taxon) { create(:taxon) }
-  let(:taxonomy) { create(:taxonomy) }
 
   scenario "商品個別ページにアクセスする。" do
     visit potepan_product_path(product.id)
-    within('.media-body') do
-      expect(page).to have_title product.name
-      expect(page).to have_content product.name
-      expect(page).to have_content product.display_price
-      expect(page).to have_content product.description
-    end
-  end
-
-  scenario "カテゴリーから商品個別ページでアクセスする。" do
-    subject
-    visit potepan_category_path(taxon_id: taxon.id, view_type: 'Grid')
-    expect(page).to have_title taxon.name
-    expect(page).to have_content product.name
-    within('.side-nav') do
-      expect(page).to have_content taxonomy.name
-      expect(page).to have_content taxonomy.taxons.first.name
-      expect(page).to have_content taxon.name
-      expect(page).to have_content taxon.product_ids.count
-    end
-    click_on product.name
     within('.media-body') do
       expect(page).to have_title product.name
       expect(page).to have_content product.name
