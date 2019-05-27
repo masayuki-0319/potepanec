831a990dd303690badf077f174a1ab4e231d0695
将之大西 <omccello@gmail.com>
2019-05-28 07:35:41 +0900

    product#showの修正に係る対応

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
+  subject { product.taxons << taxon }
+
+  let(:taxon) { create(:taxon) }
   let(:product) { create(:product) }
 
   scenario "商品個別ページにアクセスする。" do
+    subject
     visit potepan_product_path(product.id)
     within('.media-body') do
       expect(page).to have_title product.name
