eb7871ee669e7c4b2da90afe21013d844c089147
将之大西 <omccello@gmail.com>
2019-05-28 07:34:48 +0900

    指摘修正：@productsのテスト追加

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
   describe '#show' do
-    subject { get :show, params: { taxon_id: taxon.id } }
+    subject do
+      product.taxons << taxon
+      other_product.taxons << taxon
+      get :show, params: { taxon_id: taxon.id }
+    end
 
     let(:taxon) { create(:taxon) }
     let(:taxonomy) { create(:taxonomy) }
+    let(:product) { create(:product) }
+    let(:other_product) { create(:product) }
+    let(:another_product) { create(:product) }
 
     it "レスポンス成功を確認" do
       subject
@@ -25,6 +32,12 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
     it "@taxonomiesへの受け渡しを確認" do
       subject
       expect(assigns(:taxonomies)).to include(taxonomy)
+    end
+
+    it "@productsへの受け渡しを確認" do
+      subject
+      expect(assigns(:products)).to include(product, other_product)
+      expect(assigns(:products)).not_to include(another_product)
     end
   end
 end
