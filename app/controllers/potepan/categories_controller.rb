d17f9b9c46f92aedc0be46d431616d50ed7ada1c
将之大西 <omccello@gmail.com>
2019-05-30 20:24:50 +0900

    指摘修正：EagerLoadingによるN+1問題の対応

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,9 +2,9 @@ class Potepan::CategoriesController < ApplicationController
   before_action :view_type_select
 
   def show
-    @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.eager_load(:root, taxons: :children).all
     @taxon = Spree::Taxon.find_by(id: params[:taxon_id])
-    @products = @taxon.all_products
+    @products = @taxon.all_products.includes(master: [:default_price, :images])
   end
 
   private
