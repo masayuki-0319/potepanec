237de1d322645db1da029acbc85b5ec474449839
将之大西 <omccello@gmail.com>
2019-05-30 14:37:33 +0900

    指摘修正：Viewで参照したparamsを削除し，Controllerのインスタンス変数で処理を代替

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,8 +1,16 @@
 class Potepan::CategoriesController < ApplicationController
+  before_action :view_type_select
+
   def show
     @taxonomies = Spree::Taxonomy.all
     @taxon = Spree::Taxon.find_by(id: params[:taxon_id])
     @products = @taxon.all_products
-    params[:view_type] ||= 'Grid'
+  end
+
+  private
+
+  # Viewの表示において，GridとListの切り替え処理に使用する。
+  def view_type_select
+    @view_type = params[:view_type] ||= 'Grid'
   end
 end
