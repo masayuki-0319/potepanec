06ae3bab7bddce2ba23aff64b6b4b4f67713e6eb
将之大西 <omccello@gmail.com>
2019-06-07 09:39:03 +0900

    商品詳細ページの関連商品を動的に表示

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,8 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.friendly.find(params[:id])
+    taxon = Spree::Taxon.find(@product.taxon_ids.first)
+    products = taxon.all_products.includes(master: [:default_price, :images])
+    @products = products.reject { |p| p == @product }
   end
 end
