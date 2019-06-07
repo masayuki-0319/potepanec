4018e0e57ad0cd56316307b92b1ca93210f45624
将之大西 <omccello@gmail.com>
2019-06-07 19:00:49 +0900

    関連商品の表示個数を指定，owl-carouselによるスライド実装

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,7 +2,7 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.friendly.find(params[:id])
     taxon = Spree::Taxon.find(@product.taxon_ids.first)
-    products = taxon.all_products.includes(master: [:default_price, :images])
+    products = taxon.all_products.includes(master: [:default_price, :images]).shuffle.take(8)
     @products = products.reject { |p| p == @product }
   end
 end
