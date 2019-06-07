f0e2e819ddac987d25d898759cd6fa11e5e38916
将之大西 <omccello@gmail.com>
2019-06-07 11:08:13 +0900

    テスト修正：関連商品に係るテストを追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,23 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
   describe '#show' do
-    subject { get :show, params: { id: product.id } }
+    let(:taxon) { create(:taxon) }
+    let(:product) { create(:product, taxons: [taxon]) }
+    let!(:other_product) { create(:product, taxons: [taxon]) }
 
-    let(:product) { create(:product) }
+    before { get :show, params: { id: product.id } }
 
     it "レスポンス成功を確認" do
-      subject
       expect(response).to be_successful
     end
 
     it "showテンプレートの描画を確認" do
-      subject
       expect(response).to render_template :show
     end
 
     it "@productへの受け渡しを確認" do
-      subject
       expect(assigns(:product)).to eq(product)
+    end
+
+    it "@productsへの受け渡しを確認" do
+      expect(assigns(:products)).to eq [other_product]
     end
   end
 end
