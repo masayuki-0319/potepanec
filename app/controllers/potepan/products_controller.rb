33db4b85b0adb1337f597173202c71900bf9de35
将之大西 <omccello@gmail.com>
2019-06-10 16:06:09 +0900

    指摘修正：Modelを拡張して関連商品取得メソッドをControllerから転記

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,13 +1,9 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.friendly.find(params[:id])
-    products = related_products(@product).includes(master: [:default_price, :images])
-    @related_products = products.shuffle.take(8)
-  end
-
-  private
-
-  def related_products(product)
-    Spree::Product.in_taxons(product.taxons).where.not(id: product.id).distinct
+    @related_products =
+      @product.related_products.
+        includes(master: [:default_price, :images]).
+        shuffle.take(8)
   end
 end
