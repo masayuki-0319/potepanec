714452af050945c17fd8e6b62c4185f043470851
将之大西 <omccello@gmail.com>
2019-06-11 09:34:30 +0900

    指摘修正：関連商品に係るマジックナンバーの対処

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,9 +1,11 @@
 class Potepan::ProductsController < ApplicationController
+  DISPLAY_RELATED_PRODUCTS_NUMBER = 8
+
   def show
     @product = Spree::Product.friendly.find(params[:id])
     @related_products =
       @product.related_products.
         includes(master: [:default_price, :images]).
-        shuffle.take(8)
+        shuffle.take(DISPLAY_RELATED_PRODUCTS_NUMBER)
   end
 end
